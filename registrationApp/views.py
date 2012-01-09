c72db646e19fbf16b8db5079e62ba565d024e836
unknown <Darshan@Darshan-PC.(none)>
2012-01-09 00:10:23 -0500

    Search with Name, Desc, Periods[as numbers], Disc.

--- registrationApp/views.py
+++ registrationApp/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
-from registrationApp.models import Student, Course
+from registrationApp.models import Student, Course, Section, Discipline
 import re
+import pprint
 
 def index(request):
 	studentInformation= Student.objects.all()[:5]
@@ -21,5 +22,12 @@ def search(request):
 
 def searchResults(request):
 	courseOptions= Course.objects.filter(courseName__icontains=request.POST.get('search'))
+	sectionOptions= Section.objects.filter(courseID=courseOptions)
+	disciplineOptions= ([e.discipline for e in courseOptions])
+	d=([d.discipline for d in disciplineOptions])
 	results = ([c.courseName + '--' + c.courseDescription for c in courseOptions])
-	return HttpResponse('<br> '.join(results))
+	sectionResults= ([s.APeriodDays + ' ' + s.BPeriodDays + ' ' + s.CPeriodDays + ' ' + s.DPeriodDays + ' ' + s.EPeriodDays + ' ' + s.FPeriodDays + ' ' + s.GPeriodDays + ' ' +  s.HPeriodDays + ' ' + s.IPeriodDays + ' ' + s.KPeriodDays + ' ' + s.ZPeriodDays for s in sectionOptions])
+	output=[]
+	for r, sr, discipline in map(None, results, sectionResults, d):
+		output.append(str(r)+" "+str(sr)+ " "+str(discipline))
+	return HttpResponse("<br>".join(output))
