3b7607386a222c53eb9e6492ec225c378e403d01
darshandesai <darshandesai17@gmail.com>
2013-02-19 12:41:36 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -82,9 +82,6 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '$l$(8%ln$#921yx1vj3j0hmmu&f-q4=b+w$3mou10^u7v&p82r'
-
 # List of callables that know how to import templates from various sourcesself.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
